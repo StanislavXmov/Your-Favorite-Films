850d59e873a494dc0b0d5d21f6783e92833a3035
xmov <xiiiovart@gmail.com>
2021-04-14 01:33:10 +0300

    refactor(App): remove globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Global } from "@emotion/react";
 import { MainLayout } from "containers";
-import { globalStyles } from "styles";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Global styles={globalStyles} />
       <MainLayout></MainLayout>
     </div>
   );
