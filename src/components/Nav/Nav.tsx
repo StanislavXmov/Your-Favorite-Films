c5326130b2c7b20233b4785dfa3a4ef008db0468
xmov <xiiiovart@gmail.com>
2021-04-14 02:06:43 +0300

    refactor(Nav): imports

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,21 @@
-import { Container, List, Item, ItemLink, Logo } from "./Nav.styles";
+import * as Styled from "./Nav.styles";
 
 export const Nav = () => {
   return (
-    <Container>
-      <Logo />
-      <List>
-        <Item>
-          <ItemLink href="/">Home</ItemLink>
-        </Item>
-        <Item>
-          <ItemLink href="/">Favorites</ItemLink>
-        </Item>
-      </List>
-    </Container>
+    <Styled.Container>
+      <Styled.Logo />
+      <Styled.List>
+        <Styled.Item>
+          <Styled.ItemLink href="/" title="Home">
+            Home
+          </Styled.ItemLink>
+        </Styled.Item>
+        <Styled.Item>
+          <Styled.ItemLink href="/" title="Favorites">
+            Favorites
+          </Styled.ItemLink>
+        </Styled.Item>
+      </Styled.List>
+    </Styled.Container>
   );
 };
