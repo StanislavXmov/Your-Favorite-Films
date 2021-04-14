afa99550bd391c440e6aead1dda1a1b32c8f2a95
xmov <xiiiovart@gmail.com>
2021-04-15 01:03:47 +0300

    feat(UI):  add NavLink

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -2,3 +2,4 @@ export { colors } from "./theme";
 export { breakpoints } from "./theme";
 export { Logo } from "./Logo";
 export { NavButton } from "./NavButton";
+export { NavLink } from "./NavLink";
