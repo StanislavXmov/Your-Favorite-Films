a5d0997ba05f04b1991920c7accc980b6ef79075
xmov <xiiiovart@gmail.com>
2021-04-16 20:19:19 +0300

    refactor(ui): rename folder on routerLink

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,4 +1,4 @@
 export { colors } from "./theme";
 export { breakpoints } from "./theme";
 export { Logo } from "./Logo";
-export { RouterLink } from "./NavLink";
+export { RouterLink } from "./RouterLink";
