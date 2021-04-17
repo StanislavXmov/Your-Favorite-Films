dd14b510db6e3791e47399c363e4c7dd827c30e3
xmov <xiiiovart@gmail.com>
2021-04-18 01:22:27 +0300

    refactor(input):  add import

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -3,4 +3,5 @@ export { breakpoints } from "./theme";
 export { Logo } from "./Logo";
 export { RouterLink } from "./RouterLink";
 export { Input } from "./Input";
+export { Button } from "./Button";
 export { Label } from "./Label";
