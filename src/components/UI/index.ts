8f68460f3965d0d1fa6e79592b1c9040a6ee50dd
xmov <xiiiovart@gmail.com>
2021-04-18 01:14:17 +0300

    refactor(ui):  add input

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -2,3 +2,5 @@ export { colors } from "./theme";
 export { breakpoints } from "./theme";
 export { Logo } from "./Logo";
 export { RouterLink } from "./RouterLink";
+export { Input } from "./Input";
+export { Label } from "./Label";
