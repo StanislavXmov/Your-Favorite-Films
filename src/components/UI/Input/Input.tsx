e97ffd95d6d7d266f786df4d511f6b1b85f90641
xmov <xiiiovart@gmail.com>
2021-04-18 01:12:18 +0300

    refactor(input):  add props type

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export const Input = () => {
-  return <div></div>;
+import * as Styled from "./Input.styles";
+
+type Props = {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+  placeholder: string;
+};
+
+export const Input = ({ onChange, placeholder, value, type }: Props) => {
+  return (
+    <Styled.Input
+      onChange={onChange}
+      placeholder={placeholder}
+      value={value}
+      type={type}
+    />
+  );
 };
