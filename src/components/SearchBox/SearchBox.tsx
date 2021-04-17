abf86cbeb1a448602af9f4f8e922d10c9480e948
xmov <xiiiovart@gmail.com>
2021-04-17 03:12:10 +0300

    feat(searchbox):  add state and submitHandler

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 import { Container } from "components";
 
 import * as Styled from "./SearchBox.styles";
 
 export const SearchBox = () => {
+  const [title, setTitle] = useState("");
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (title.trim()) {
+      console.log(title);
+    }
+  };
   return (
     <Styled.Section>
       <Container>
-        <Styled.Form>
+        <Styled.Form onSubmit={(event) => submitHandler(event)}>
           <Styled.Label>Search Film</Styled.Label>
-          <Styled.Input type="text" placeholder="Enter film title" />
+          <Styled.Input
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            type="text"
+            placeholder="Enter film title"
+          />
         </Styled.Form>
       </Container>
     </Styled.Section>
