155f03fb765484ef12f334136b7d26ed1cf30b20
xmov <xiiiovart@gmail.com>
2021-04-18 01:30:57 +0300

    refactor(searchBox): add ui components

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { Container } from "components";
+import { Label, Input, Button } from "components/UI";
 
 import * as Styled from "./SearchBox.styles";
 
@@ -16,16 +17,14 @@ export const SearchBox = () => {
     <Styled.Section>
       <Container>
         <Styled.Form onSubmit={(event) => submitHandler(event)}>
-          <Styled.Label>Search Film</Styled.Label>
-          <Styled.Input
+          <Label title="Search Film" />
+          <Input
             value={title}
             onChange={(event) => setTitle(event.target.value)}
             type="text"
             placeholder="Enter film title"
           />
-          <Styled.Button disabled={!title} type="submit">
-            Find
-          </Styled.Button>
+          <Button disabled={!title} type="submit" title="find" />
         </Styled.Form>
       </Container>
     </Styled.Section>
