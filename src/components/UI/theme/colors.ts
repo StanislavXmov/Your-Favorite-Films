40cd397b95ab1b0c0102ed2464ca7925567affda
xmov <xiiiovart@gmail.com>
2021-04-17 03:17:34 +0300

    style(ui): add backgroundColorGrey

--- src/components/UI/theme/colors.ts
+++ src/components/UI/theme/colors.ts
@@ -1,6 +1,7 @@
 export const colors = {
   backgroundColor: "#654062",
   backgroundColorLighter: "#915c8c",
+  backgroundColorGrey: "#333333",
   logoGrey: "#F4F4F4",
   logoYellow: "#FFD66B",
   logoOrange: "#FF9D72",
