81dc5ef44063b64b9412fc1e8c297b0976ce75ed
xmov <xiiiovart@gmail.com>
2021-04-21 02:11:20 +0300

    feat(app):  add store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+
 import "normalize.css";
-import "./index.css";
+
+import { search } from "./store/reducers";
 import App from "./App";
+import "./index.css";
+
+const rootReducer = combineReducers({ search });
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
