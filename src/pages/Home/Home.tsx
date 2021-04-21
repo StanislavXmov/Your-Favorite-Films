cb124ae25163bd40b5c00bbb67a0dc249fd1c464
xmov <xiiiovart@gmail.com>
2021-04-22 02:40:18 +0300

    feat(home): add mock loader and mapping films

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
+import { useSelector, TypedUseSelectorHook } from "react-redux";
 import { SearchBox } from "components";
+import { RootState } from "index";
+
+const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const Home = () => {
+  const { loading, films } = useTypeSelector((state) => state.search);
   return (
     <>
       <SearchBox />
+      {loading && <h2>Loading</h2>}
+      {films && films.map((f) => <p key={f.id}>{f.title}</p>)}
     </>
   );
 };
