22e1d26eb4af2b79bde1c542bf3e5bd6ae69eb1d
xmov <xiiiovart@gmail.com>
2021-04-28 01:31:08 +0300

    refactor(store):  add type.ts

--- src/store/state/state.ts
+++ src/store/state/state.ts
@@ -1,31 +1,4 @@
-export type SearchState = {
-  films: Film[];
-  searchResponse?: ResponseApi;
-  error?: ResponseError;
-  loading: boolean;
-};
-
-export type ResponseApi = {
-  page: number;
-  results: Film[];
-  totalPages: number;
-  totalResults: number;
-};
-
-export type ResponseError = {
-  message: string;
-};
-
-export type Film = {
-  backdropPath: string;
-  id: number;
-  originalTitle: string;
-  overview: string;
-  posterPath: string;
-  releaseDate: string;
-  title: string;
-  voteAverage: number;
-};
+import { SearchState } from "./type";
 
 export const initialState: SearchState = {
   films: [],
---
+++ src/store/state/type.ts
@@ -0,0 +1,28 @@
+export type SearchState = {
+  films: Film[];
+  searchResponse?: ResponseApi;
+  error?: ResponseError;
+  loading: boolean;
+};
+
+export type ResponseApi = {
+  page: number;
+  results: Film[];
+  totalPages: number;
+  totalResults: number;
+};
+
+export type ResponseError = {
+  message: string;
+};
+
+export type Film = {
+  backdropPath: string;
+  id: number;
+  originalTitle: string;
+  overview: string;
+  posterPath: string;
+  releaseDate: string;
+  title: string;
+  voteAverage: number;
+};
